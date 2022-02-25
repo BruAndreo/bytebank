80297123bb085263443ab8da5e0c252fd7a9832d
Bruno Andreo <brunolino2026@gmail.com>
2022-02-24 21:06:35 -0300

    Finished basic function for Conta class

--- src/main.kt
+++ src/main.kt
@@ -1,31 +1,33 @@
 fun main() {
     println("Bem vindo ao ByteBank")
 
-    val contaBruno = Conta()
-    contaBruno.titular = "Bruno"
-    contaBruno.numero = 12345
+    val contaBruno = Conta("Bruno", 12345)
     contaBruno.depositar(100.0)
 
     println("${contaBruno.titular} | Conta: ${contaBruno.numero}")
     println("Seu saldo é de: R$ ${contaBruno.saldo}")
 
-    contaBruno.sacar(150.0)
-
     contaBruno.sacar(50.2)
     println("Seu saldo é de: R$ ${contaBruno.saldo}")
+
+    val contaGeovana = Conta("Geovana", 12346)
+
+    contaBruno.transferencia(contaGeovana, 40.0)
+
+    println("Saldo da conta da Geovana é de R$ ${contaGeovana.saldo}")
+    println("Saldo da conta do Bruno é de R$ ${contaBruno.saldo}")
 }
 
-class Conta {
-    var titular = ""
-    var numero = 0
+class Conta(val titular: String, val numero: Int) {
     var saldo = 0.0
+        private set
 
     fun depositar(valor: Double) {
         saldo += valor
     }
 
     fun sacar(valor: Double) {
-        if (saldoEstaNegativo()) {
+        if (!saldoEhSuficiente(valor)) {
             println("Saldo insuficiente")
             return
         }
@@ -34,7 +36,18 @@ class Conta {
         println("Saque efetuado com sucesso")
     }
 
-    private fun saldoEstaNegativo(): Boolean {
-        return saldo <= 0.0;
+    fun transferencia(contaDestino: Conta, valor: Double) {
+        if (!saldoEhSuficiente(valor)) {
+            println("Saldo insuficiente")
+            return
+        }
+
+        saldo -= valor
+        contaDestino.depositar(valor)
+        println("Transferencia realizada para Conta ${contaDestino.numero}")
+    }
+
+    private fun saldoEhSuficiente(valor: Double): Boolean {
+        return saldo >= valor
     }
 }
